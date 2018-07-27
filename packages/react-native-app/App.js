d7d547e0868b83ed6e17822005ade584413a2ff0
Puthranps <puthranpreethesh@gmail.com>
2018-07-26 21:20:10 -0400

    Replace react-native folder with create-react-native

--- packages/react-native-app/App.js
+++ packages/react-native-app/App.js
@@ -1,12 +1,23 @@
-import React, {Component} from 'react'; 
-import { Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
 
-export default class App extends Componet {
+export default class App extends React.Component {
   render() {
     return (
-      <View>
-        <Text> Testing React Native </Text>
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+        <Text>Changes you make will automatically reload.</Text>
+        <Text>Shake your phone to open the developer menu.</Text>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
